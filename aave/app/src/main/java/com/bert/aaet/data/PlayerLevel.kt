209746ea6b2cbe0304fd6da063f1d24a5b16eab8
Bertking <kingcheeng163@gmail.com>
2020-12-31 17:40:38 +0800

    define player level
    使用枚举类来描述玩家的游戏水平

---
+++ aave/app/src/main/java/com/bert/aaet/data/PlayerLevel.kt
@@ -0,0 +1,34 @@
+package com.bert.aaet.data
+
+/**
+ *
+ * @Author: bertking
+ * @ProjectName: aaet
+ * @CreateAt: 2020/12/31 5:01 PM
+ * @UpdateAt: 2020/12/31 5:01 PM
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ * @Description: LOL 的玩家水平
+ */
+enum class PlayerLevel {
+    //最强王者
+    THE_STRONGEST_KING,
+
+    //超凡大师
+    MASTER_CHALLENGER,
+
+    // 璀璨钻石
+    BRIGHT_DIAMOND,
+
+    //华贵铂金
+    LUXURIOUS_PLATINUM,
+
+    //荣耀黄金：
+    THE_GLORY_OF_THE_GOLD,
+
+    //不屈白银
+    UNYIELDING_SILVER,
+
+    //英勇青铜
+    THE_HEROIC_BRONZE
+}

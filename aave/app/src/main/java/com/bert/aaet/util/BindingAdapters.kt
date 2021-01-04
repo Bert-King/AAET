9b1385ffc2756c9c9d2288a6f80bdf03939c268e
Bertking <kingcheeng163@gmail.com>
2021-01-04 19:53:37 +0800

    增加玩家的段位说明

--- aave/app/src/main/java/com/bert/aaet/util/BindingAdapters.kt
+++ aave/app/src/main/java/com/bert/aaet/util/BindingAdapters.kt
@@ -3,9 +3,11 @@ package com.bert.aaet.util
 import android.content.Context
 import android.graphics.drawable.Drawable
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.bert.aaet.R
+import com.bert.aaet.data.PlayerLevel
 
 /**
  *
@@ -30,3 +32,39 @@ private fun getGenderDrawable(gender: Boolean, context: Context): Drawable? {
 
     }
 }
+
+
+@BindingAdapter("app:levelText")
+fun levelText(view: TextView, level: PlayerLevel) {
+    view.text = getLevelText(level,view.context)
+}
+
+private fun getLevelText(level: PlayerLevel, context: Context): String {
+    return when (level) {
+        PlayerLevel.THE_HEROIC_BRONZE -> {
+            context.getString(R.string.heroic_bronze)
+        }
+
+        PlayerLevel.UNYIELDING_SILVER -> {
+            context.getString(R.string.unyielding_silver)
+        }
+
+        PlayerLevel.THE_GLORY_OF_THE_GOLD -> {
+            context.getString(R.string.glory_of_the_gold)
+        }
+
+        PlayerLevel.LUXURIOUS_PLATINUM -> {
+            context.getString(R.string.luxurious_platinum)
+
+        }
+
+        PlayerLevel.MASTER_CHALLENGER -> {
+            context.getString(R.string.master_challenger)
+        }
+
+        else -> {
+            context.getString(R.string.strongest_king)
+        }
+    }
+}
+

0833840dbef4efb50af84b1c9f3f57b88f3466e4
Bertking <kingcheeng163@gmail.com>
2021-01-07 20:43:23 +0800

    Add DashboardFragment

---
+++ aave/app/src/main/java/com/bert/aaet/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,42 @@
+package com.bert.aaet.ui.dashboard
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.bert.aaet.ConstraintActivity
+import com.bert.aaet.PlayerActivity
+import com.bert.aaet.R
+
+class DashboardFragment : Fragment() {
+
+    private lateinit var dashboardViewModel: DashboardViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dashboardViewModel =
+            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    fun go2Player(view: View) {
+        startActivity(Intent(context, PlayerActivity::class.java))
+    }
+
+    fun go2Constraint(view: View) {
+        startActivity(Intent(context, ConstraintActivity::class.java))
+    }
+}

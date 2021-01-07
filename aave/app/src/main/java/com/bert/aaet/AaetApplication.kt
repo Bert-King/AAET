d9aa85a75da7ed173ea8ac8b63bf9add02756559
Bertking <kingcheeng163@gmail.com>
2021-01-07 14:32:34 +0800

    Usint Hilt in my application

---
+++ aave/app/src/main/java/com/bert/aaet/AaetApplication.kt
@@ -0,0 +1,21 @@
+package com.bert.aaet
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ *
+ * @Author: bertking
+ * @ProjectName: aaet
+ * @CreateAt: 2021/1/7 2:26 PM
+ * @UpdateAt: 2021/1/7 2:26 PM
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ * @Description:
+ */
+@HiltAndroidApp
+class AaetApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}

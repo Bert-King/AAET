17892f24ab47e3bf090d7446fd5e8e939d498e59
Bertking <kingcheeng163@gmail.com>
2021-01-08 17:12:59 +0800

    Create an CarFragment

---
+++ aave/app/src/main/java/com/bert/aaet/ui/car/CarFragment.kt
@@ -0,0 +1,32 @@
+package com.bert.aaet.ui.car
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bert.aaet.R
+
+class CarFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = CarFragment()
+    }
+
+    private lateinit var viewModel: CarViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.car_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(CarViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ aave/app/src/main/java/com/bert/aaet/ui/car/CarViewModel.kt
@@ -0,0 +1,7 @@
+package com.bert.aaet.ui.car
+
+import androidx.lifecycle.ViewModel
+
+class CarViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}

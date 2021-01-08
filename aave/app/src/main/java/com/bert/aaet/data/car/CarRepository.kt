b33478615484734e9527959e8ffb9fcdbd06c4f4
Bertking <kingcheeng163@gmail.com>
2021-01-08 17:13:04 +0800

    Create CarRepository.kt

---
+++ aave/app/src/main/java/com/bert/aaet/data/car/CarRepository.kt
@@ -0,0 +1,14 @@
+package com.bert.aaet.data.car
+
+/**
+ *
+ * @Author: bertking
+ * @ProjectName: aaet
+ * @CreateAt: 2021/1/8 5:11 PM
+ * @UpdateAt: 2021/1/8 5:11 PM
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ * @Description:
+ */
+class CarRepository {
+}

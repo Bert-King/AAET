c958e135d05cfc1e846ee6ded19bcbaf1b8bf9f0
Bertking <kingcheeng163@gmail.com>
2021-01-08 17:12:45 +0800

    Create CarDatabase.kt

---
+++ aave/app/src/main/java/com/bert/aaet/data/car/CarDatabase.kt
@@ -0,0 +1,45 @@
+package com.bert.aaet.data.car
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+/**
+ *
+ * @Author: bertking
+ * @ProjectName: aaet
+ * @CreateAt: 2021/1/8 3:51 PM
+ * @UpdateAt: 2021/1/8 3:51 PM
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ * @Description: 构建Database
+ *
+ * https://developer.android.com/codelabs/android-room-with-a-view-kotlin#7
+ */
+@Database(entities = arrayOf(Car::class),version = 1,exportSchema = false)
+public abstract class CarDatabase :RoomDatabase() {
+    abstract fun carDao():CarDao
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: CarDatabase? = null
+
+        fun getDatabase(context: Context): CarDatabase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    CarDatabase::class.java,
+                    "car_database"
+                ).build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
+}
